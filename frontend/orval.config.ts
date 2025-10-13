1d374079d37e931cc11c6979d704ca201d6d525b
YASHDEV42 <yahyashannat@gmail.com>
2025-10-13 18:59:17 +0300

    adding apiFetcher for orval and deleting api-client for improving and making new api-client for every tag

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -3,8 +3,13 @@ module.exports = {
   yshai: {
     input: "http://localhost:5000/api-json",
     output: {
-      target: "src/lib/api-client.ts",
+      target: "/lib/api-client.ts",
       client: "fetch",
+      override: {
+        fetch: {
+          implementation: "./lib/apiFetch.ts"
+        }
+      }
     },
   },
 };
